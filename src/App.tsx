ecdadadfc9169fd07b328690eb03a876160c5e7f
oveddan <stangogh@gmail.com>
2021-08-27 11:31:32 -0400

    water example

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import "./App.css";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
-import { PerspectiveCamera } from "@react-three/drei";
+import { Mesh, PerspectiveCamera as ThreePerspectiveCamera, PlaneBufferGeometry } from "three";
+import { OrbitControls, PerspectiveCamera, useTexture } from "@react-three/drei";
+import { extend, Object3DNode } from '@react-three/fiber'
+import { Water2Options } from "three-stdlib";
+import {Water} from 'three/examples/jsm/objects/Water2';
+import WaterNormalA from './Water_1_M_Normal.jpeg';
+import WaterNormalB from './Water_2_M_Normal.jpeg';
+
+extend({Water});
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'water': Object3DNode<Water, typeof Water>;
+    }
+  }
+}
 
 const Box = (props: { position: [number, number, number] }) => {
   // This reference will give us direct access to the THREE.Mesh object
@@ -30,7 +45,29 @@ const Box = (props: { position: [number, number, number] }) => {
   );
 };
 
+const geo = new PlaneBufferGeometry(100,100);
+
+const WaterWrapper = () => {
+ const waterNormalTextureA = useTexture(WaterNormalA);
+  const waterNormalTextureB = useTexture(WaterNormalB);
+
+const options: Water2Options = {
+  color: 'white',
+  normalMap0: waterNormalTextureA,
+  normalMap1: waterNormalTextureB,
+  reflectivity: 0.1,
+  // flowSpeed: 0.1,
+  // scale: 0.5
+
+};
+
+  return <water rotation-x={-Math.PI/2} position-y={-1} args={[geo, options]} />
+}
+
 function App() {
+ 
+  const myCamera = useRef<ThreePerspectiveCamera>();
+
   return (
     <div style={{ width: 500, height: 500 }}>
       <Canvas>
@@ -38,15 +75,22 @@ function App() {
           makeDefault
           position-z={10}
           position-y={2}
+          ref={myCamera}
         ></PerspectiveCamera>
+        <OrbitControls camera={myCamera.current} />
 
-        <ambientLight args={["white", 0.2]} />
+        <ambientLight args={["white", 1]} />
         <pointLight position={[10, 10, 10]} />
         <Box position={[-1.2, 0, 0]} />
         <Box position={[1.2, 0, 0]} />
-        <mesh position={[0, 0, -5]} rotation-x={1}>
+        {/* <mesh position={[0, 0, -5]} rotation-x={1}>
           <planeGeometry args={[100, 100]} />
           <meshLambertMaterial color="blue" />
+        </mesh> */}
+        <Suspense fallback={null}><WaterWrapper /></Suspense>
+        <mesh position-y={-10}rotation-x={-Math.PI/2} >
+          <planeBufferGeometry args={[100,100]} />
+          <meshPhongMaterial color="blue" />
         </mesh>
       </Canvas>
     </div>
